75708891ed598f3c1ecb26d64b55302e9f34c41d
awesomer <61159417+floober-doober@users.noreply.github.com>
2025-11-10 13:10:50 +0000

    get class and a few errors

--- src/server/api/routers/classes.ts
+++ src/server/api/routers/classes.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import {
@@ -33,6 +34,36 @@ export const classesRouter = createTRPCRouter({
 		});
 
 		return classes;
+	}),
+
+	getClass: protectedProcedure.input(z.object({ classId: z.number().int().positive() })).query(async ({ input: { classId }, ctx }) => {
+		const user = ctx.session.user;
+
+		const classResult = await ctx.db.class.findUnique({
+			where: {
+				id: classId
+			},
+			include: {
+				createdBy: {
+					select: {
+						id: true,
+						name: true,
+					},
+				},
+			},
+		});
+
+		if (!classResult)
+			throw new TRPCError({
+				code: "NOT_FOUND"
+			})
+
+		if (classResult.createdById !== user.id)
+			throw new TRPCError({
+				code: "UNAUTHORIZED"
+		})
+
+		return classResult;
 	}),
 
 	createClass: protectedProcedure
