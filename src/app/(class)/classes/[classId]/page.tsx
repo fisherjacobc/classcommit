bddbd25c0d9e8f45fbdc3c3e392cdc2dea4a01b4
Fisher <72705185+fisherjacobc@users.noreply.github.com>
2026-05-01 05:21:46 -0400

    class homepage

--- src/app/(class)/classes/[classId]/page.tsx
+++ src/app/(class)/classes/[classId]/page.tsx
@@ -5,12 +5,18 @@ export default async function Classes({ params }: { params: Promise<{ classId: s
     const { classId } = await params;
 
     const classData = await api.classes.getClass({ classId: Number.parseInt(classId, 10) });
+    const homepageMarkdown = await api.classes.getHomepage({ classId: Number.parseInt(classId, 10) });
 
     return (
         <HydrateClient>
-            <main className="mx-4 flex flex-col gap-y-12">
-                <span className="font-bold text-4xl">{classData.name}</span>
-                <Markdown></Markdown>
+            <main className="mx-4 flex flex-col gap-y-6">
+                <div className="flex flex-col border-b">
+                    <span className="font-bold text-4xl">{classData.name}</span>
+                    <span className="text-gray-500 text-lg">Join Code: {classData.joinCode}</span>
+                </div>
+                <div id="markdown" className="flex flex-col gap-y-1.5">
+                    <Markdown>{homepageMarkdown}</Markdown>
+                </div>
             </main>
         </HydrateClient>
     )
